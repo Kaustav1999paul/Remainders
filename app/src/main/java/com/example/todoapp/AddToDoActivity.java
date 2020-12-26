a8be579ecedbbccd2eeacbf2778a362764c1986c
Kaustav1999paul <codeit1999@gmail.com>
2020-12-27 01:32:03 +0530

    --- Fourth Commit ---
    Home Activity
    Add ToDo Activity
    Account Activity
    ----------------------------
    Home Screen Activity Modified
    ToDo Activity designed
    Account Activity added

--- app/src/main/java/com/example/todoapp/AddToDoActivity.java
+++ app/src/main/java/com/example/todoapp/AddToDoActivity.java
@@ -1,17 +1,42 @@
 package com.example.todoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddToDoActivity extends AppCompatActivity {
 
     private ImageView close;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private EditText title, description;
+    private FloatingActionButton ok;
+    String tit, desc, id, postState;
+    private FirebaseUser user;
+    Map<String, Object> doc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +47,126 @@ public class AddToDoActivity extends AppCompatActivity {
                     WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        ok = findViewById(R.id.ok);
+        id = getIntent().getStringExtra("id");
+        postState = getIntent().getStringExtra("postState");
+        title = findViewById(R.id.title);
+        description = findViewById(R.id.desc);
         close = findViewById(R.id.close);
         close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+                Intent intent = new Intent(AddToDoActivity.this, HomeActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
             }
         });
+
+        if (postState.equals("new")){
+            ok.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    tit = title.getText().toString().trim();
+                    desc = description.getText().toString();
+
+                    if (TextUtils.isEmpty(tit)){
+                        Toast.makeText(AddToDoActivity.this, "Empty", Toast.LENGTH_SHORT).show();
+                    }else {
+                        uploadPost(tit, desc);
+                    }
+                }
+            });
+        }
+        else {
+            ok.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    tit = title.getText().toString().trim();
+                    desc = description.getText().toString();
+
+                    if (TextUtils.isEmpty(tit)){
+                        Toast.makeText(AddToDoActivity.this, "Empty", Toast.LENGTH_SHORT).show();
+                    }else {
+                        updatePost(tit, desc);
+                    }
+                }
+            });
+            getData();
+        }
+    }
+
+    private void updatePost(String tit, String desc) {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd MMM, YYYY");
+        String saveCurrentDate = currentDate.format(calendar.getTime());
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        String saveCurrentTime = currentTime.format(calendar.getTime());
+
+        String idd = saveCurrentDate + saveCurrentTime + user.getUid();
+
+        Map<String, Object> userdataMap = new HashMap<>();
+        userdataMap.put("id", idd);
+        userdataMap.put("title", tit);
+        userdataMap.put("description", desc);
+        userdataMap.put("state", "completed");
+        userdataMap.put("date", saveCurrentDate);
+        userdataMap.put("time", saveCurrentTime);
+
+        db.collection("Users").document(user.getUid()).collection("Records")
+                .document(id).update(userdataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Intent intent = new Intent(AddToDoActivity.this, HomeActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                Toast.makeText(AddToDoActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getData() {
+        db.collection("Users").document(user.getUid()).collection("Records").document(id).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    doc = (Map<String, Object>) task.getResult().getData();
+                    title.setText(doc.get("title").toString());
+                    description.setText(doc.get("description").toString());
+                }
+            }
+        });
+    }
+
+    private void uploadPost(String tit, String desc) {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd MMM, YYYY");
+        String saveCurrentDate = currentDate.format(calendar.getTime());
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        String saveCurrentTime = currentTime.format(calendar.getTime());
+
+        String idd = saveCurrentDate + saveCurrentTime + user.getUid();
+
+        Map<String, Object> userdataMap = new HashMap<>();
+        userdataMap.put("id", idd);
+        userdataMap.put("title", tit);
+        userdataMap.put("description", desc);
+        userdataMap.put("state", "completed");
+        userdataMap.put("date", saveCurrentDate);
+        userdataMap.put("time", saveCurrentTime);
+
+        db.collection("Users").document(user.getUid()).collection("Records")
+                .document(idd).set(userdataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Intent intent = new Intent(AddToDoActivity.this, HomeActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                Toast.makeText(AddToDoActivity.this, "Posted", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/todoapp/HomeActivity.java
+++ app/src/main/java/com/example/todoapp/HomeActivity.java
@@ -1,23 +1,39 @@
 package com.example.todoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.view.ViewCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -25,7 +41,11 @@ public class HomeActivity extends AppCompatActivity {
     private TextView holderName;
     private FirebaseUser user;
     private String photo, name;
-    private CardView addTodo;
+    private CardView account;
+    RecyclerView.Adapter<TodoVH> adapter;
+    RecyclerView list;
+    List<QueryDocumentSnapshot> documentSnapshots = new ArrayList<>();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +57,8 @@ public class HomeActivity extends AppCompatActivity {
                     WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
 
-        addTodo = findViewById(R.id.addToDo);
+        list = findViewById(R.id.list);
+        account = findViewById(R.id.cardImage);
         holderImage = findViewById(R.id.HolderImage);
         abc = findViewById(R.id.abc);
         holderName = findViewById(R.id.HolderName);
@@ -48,14 +69,93 @@ public class HomeActivity extends AppCompatActivity {
         holderName.setText(name);
         Glide.with(holderImage).load(photo).into(holderImage);
 
-        addTodo.setOnClickListener(new View.OnClickListener() {
+        abc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomeActivity.this, AddToDoActivity.class);
-                ActivityOptionsCompat optionsCompat = ActivityOptionsCompat
-                        .makeSceneTransitionAnimation(HomeActivity.this, addTodo, ViewCompat.getTransitionName(abc));
-                startActivity(intent, optionsCompat.toBundle());
+                intent.putExtra("postState", "new");
+                startActivity(intent);
             }
         });
+
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, AccountActivity.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(account, "share_image");
+                pairs[1] = new Pair<View, String>(holderName, "share_name");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(HomeActivity.this, pairs);
+                intent.putExtra("photo", photo);
+                intent.putExtra("name", user.getDisplayName());
+                startActivity(intent, options.toBundle());
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        adapter = new RecyclerView.Adapter<TodoVH>() {
+            @NonNull
+            @Override
+            public TodoVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return new TodoVH(LayoutInflater.from(parent.getContext()).inflate(R.layout.to_do_layout, parent, false));
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull TodoVH holder, final int position) {
+                String a = String.valueOf(documentSnapshots.get(position).getData().get("title"));
+                String b = String.valueOf(documentSnapshots.get(position).getData().get("description"));
+                String id = String.valueOf(documentSnapshots.get(position).getData().get("id"));
+
+                holder.title.setText(a.substring(0, Math.min(a.length(), 20)));
+                holder.body.setText(b);
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(HomeActivity.this, AddToDoActivity.class);
+                        intent.putExtra("id", id);
+                        intent.putExtra("postState", "old");
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @Override
+            public int getItemCount() {
+                return documentSnapshots.size();
+            }
+        };
+
+        db.collection("Users").document(user.getUid()).collection("Records")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot doc : task.getResult()){
+                                documentSnapshots.add(doc);
+                            }
+                                list.setAdapter(adapter);
+                                list.setLayoutManager(new GridLayoutManager(HomeActivity.this, 2));
+
+                        }
+                    }
+                });
+
+    }
+
+    public class TodoVH extends RecyclerView.ViewHolder{
+        TextView title, body;
+        public TodoVH(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.titleToDo);
+            body = itemView.findViewById(R.id.bodyToDo);
+        }
     }
 }
